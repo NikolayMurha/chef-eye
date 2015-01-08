0de207e58a722fa45de543ab9edf344ed6c489d7
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-01-08 15:44:59 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nikolay.m@randrmusic.com'
 license          'Apache v2.0'
 description      'Installs/Configures chef_eye'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 
 recipe 'chef_eye',  'Installs all'
